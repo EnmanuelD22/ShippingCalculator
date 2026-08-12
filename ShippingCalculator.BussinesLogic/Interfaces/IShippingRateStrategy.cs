62a682f44b24da7aeec7acecee6c4022a1208b9c
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-08-12 16:46:24 -0400

    Eliminar archivos temporales y configurar .gitignore

--- ShippingCalculator.BussinesLogic/Interfaces/IShippingRateStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace ShippingCalculator.BusinessLogic.Interfaces
-{
-    public interface IShippingRateStrategy
-    {
-        string CountryCode { get; }
-        decimal CalculateRate(decimal weight, decimal baseRate);
-    }
-}
