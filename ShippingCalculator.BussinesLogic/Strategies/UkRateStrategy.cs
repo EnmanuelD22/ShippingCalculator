e759c25665a3c157d5191bd5db73ef7f7c9838a1
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-08-12 16:46:37 -0400

    Finalizar cambios

---
+++ ShippingCalculator.BussinesLogic/Strategies/UkRateStrategy.cs
@@ -0,0 +1,15 @@
+﻿
+using ShippingCalculator.BusinessLogic.Interfaces;
+
+namespace ShippingCalculator.BusinessLogic.Strategies
+{
+    public class UkRateStrategy : IShippingRateStrategy
+    {
+        public string CountryCode => "UK";
+
+        public decimal CalculateRate(decimal weight, decimal baseRate)
+        {
+            return weight * baseRate;
+        }
+    }
+}
---
+++ ShippingCalculator.BussinesLogic/Strategies/UsRateStrategy.cs
@@ -0,0 +1,14 @@
+﻿using ShippingCalculator.BusinessLogic.Interfaces;
+
+namespace ShippingCalculator.BusinessLogic.Strategies
+{
+    public class UsRateStrategy : IShippingRateStrategy
+    {
+        public string CountryCode => "US";
+
+        public decimal CalculateRate(decimal weight, decimal baseRate)
+        {
+            return weight * baseRate;
+        }
+    }
+}
